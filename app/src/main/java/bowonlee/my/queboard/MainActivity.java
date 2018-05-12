d91a28bbfd61ebb1713fe02b46b3087e8c966ec1
bowon <asdfg3678@gmail.com>
2018-05-13 01:26:57 +0900

    asdasd

--- app/src/main/java/bowonlee/my/queboard/MainActivity.java
+++ app/src/main/java/bowonlee/my/queboard/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private Button messageButton;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
